599968f5ad9a0a9894c811ffe27be07a077f6968
Yeil Kim <yeilkim2016@gmail.com>
2020-10-14 13:38:07 -0400

    map file turned to 2d array

--- game.js
+++ game.js
@@ -2,12 +2,21 @@ import * as fs from 'fs'
 
 let x = 13;
 let y = 13;
+let board = [];
 
 
 export const levelBuild = function () {
+    //
     let level = fs.readFileSync("./levels/Level 2.map").toString()//.split("/n")
-    console.log(level.charAt(14));
-    return level;
+    let count = 0;
+    for (let i = 0; i < y; i++) {
+        board[i] = [];
+        for (let j = 0; j < x; j++) {
+            board[i][j] = level.charAt(count);
+            count ++;
+        }
+        count += 2;
+    }
+    return board;
 }
-
 console.log(levelBuild());
