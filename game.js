c4f89044289dec12459059eb2f5ec7a96cec109e
Yeil Kim <yeilkim2016@gmail.com>
2020-11-08 17:02:19 -0500

    implemented simple stopwatch

--- game.js
+++ game.js
@@ -9,8 +9,14 @@ let board = [];
 
 let player = {
     x: 0,
-    y: 0
+    y: 0,
+    moves: 0, //May add to backend as part of scoreboard
+    time: 0,
+    won: false // If true, stop the game make something pop up
 }
+
+//Backend either have text file of mazes or stored as 2d array to save processing time
+//Store minimum moves required to run checker;  
 
 board = [
     [
@@ -92,8 +98,15 @@ board = [
         '0'
     ]
 ]
+let minMove = 3; //one less than winning move, different for all boards
+
+let speed = 10;
 
 let doneMove = true;
+
+let firstMove = true;
+
+let stopWatch;
 
 $(function () {
     loadGame();
@@ -103,42 +116,69 @@ export async function loadGame() {
     const $root = $('#root')
     $root.append(levelBuild())
 
+
     $(document).keydown(async function (e) {
         switch (e.keyCode) { // move to seperate function and make it return something
             case 39:
                 e.preventDefault;
-                if (doneMove) {
-                    doneMove = false;
-                    await move('right', doneMove);
+                if (!player.won) {
+                    if (firstMove) {
+                        stopWatch = Date.now();
+                        firstMove = false;
+                    }
+                    if (doneMove) {
+                        doneMove = false;
+                        player.moves += 1
+                        await move('right');
+                    }
                 }
-
+                
                 break;
             case 38:
                 e.preventDefault;
-                if (doneMove) {
-                    doneMove = false;
-                    await move('up', doneMove);
+                if (!player.won) {
+                    if (firstMove) {
+                        stopWatch = Date.now();
+                        firstMove = false;
+                    }
+                    if (doneMove) {
+                        doneMove = false;
+                        player.moves += 1
+                        await move('up');
+                    }
                 }
                 break;
             case 37:
-                e.preventDefault;
-                if (doneMove) {
-                    doneMove = false;
-                    await move('left', doneMove);
+                if (!player.won) {
+                    if (firstMove) {
+                        stopWatch = Date.now();
+                        firstMove = false;
+                    }
+                    if (doneMove) {
+                        doneMove = false;
+                        player.moves += 1
+                        await move('left');
+                    }
                 }
                 break;
             case 40:
                 e.preventDefault;
-                if (doneMove) {
-                    doneMove = false;
-                    await move('down', doneMove);
+                if (!player.won) {
+                    if (firstMove) {
+                        stopWatch = Date.now();
+                        firstMove = false;
+                    }
+                    if (doneMove) {
+                        doneMove = false;
+                        player.moves += 1
+                        await move('down');
+                    }
                 }
                 break;
         }
     })
+
 }
-
-
 
 export const levelBuild = function () {
 
@@ -196,21 +236,29 @@ export async function move(dirction) {
     let findY;
     let findX;
 
-    let speed = 10;
-
     if (dirction == 'right') {
         let test = setInterval(function animate() {
             // color what ever space that color
             // Depending on how it is animated and logic; move one space at a time
-
             $(findY).find(findX).attr('id', '')
 
             if (board[player.y][player.x + 1] == 0) {
                 board[player.y][player.x] = 2;
                 findY = '.' + player.y;
                 findX = '.' + player.x;
-                clearInterval(test)
+
                 $(findY).find(findX).attr('id', 'player')
+                clearInterval(test)
+                if (player.moves >= minMove) {
+                    player.won = boardChecker()
+                    if (player.won) {
+                        console.log('Won')
+                        stopWatch = Date.now() - stopWatch;
+                        player.time = stopWatch/1000;
+                        console.log(player.time);
+                    }
+                }
+
                 return doneMove = true;
             } else if (board[player.y][player.x] >= 1) {
                 board[player.y][player.x] = 2;
@@ -232,8 +280,19 @@ export async function move(dirction) {
                 board[player.y][player.x] = 2;
                 findY = '.' + player.y;
                 findX = '.' + player.x;
-                clearInterval(test)
+
                 $(findY).find(findX).attr('id', 'player')
+                clearInterval(test)
+                if (player.moves >= minMove) {
+                    player.won = boardChecker()
+                    if (player.won) {
+                        console.log('Won')
+                        stopWatch = Date.now() - stopWatch;
+                        player.time = stopWatch/1000;
+                        console.log(player.time);
+                    }
+                }
+
                 return doneMove = true;
             } else if (board[player.y][player.x] >= 1) {
                 board[player.y][player.x] = 2;
@@ -254,8 +313,18 @@ export async function move(dirction) {
                 board[player.y][player.x] = 2;
                 findY = '.' + player.y;
                 findX = '.' + player.x;
-                clearInterval(test)
+
                 $(findY).find(findX).attr('id', 'player')
+                clearInterval(test)
+                if (player.moves >= minMove) {
+                    player.won = boardChecker()
+                    if (player.won) {
+                        console.log('Won')
+                        stopWatch = Date.now() - stopWatch;
+                        player.time = stopWatch/1000;
+                        console.log(player.time);                    }
+                }
+
                 return doneMove = true;
             } else if (board[player.y][player.x] >= 1) {
                 board[player.y][player.x] = 2;
@@ -277,8 +346,18 @@ export async function move(dirction) {
                 findY = '.' + player.y;
                 findX = '.' + player.x;
                 //$(findY).find(findX).attr('class', player.x + ' filled')
-                clearInterval(test)
+
                 $(findY).find(findX).attr('id', 'player')
+                clearInterval(test)
+                if (player.moves >= minMove) {
+                    player.won = boardChecker();
+                    if (player.won) {
+                        console.log('Won')
+                        stopWatch = Date.now() - stopWatch;
+                        player.time = stopWatch/1000;
+                        console.log(player.time);                    }
+                }
+
                 return doneMove = true;
             } else if (board[player.y][player.x] >= 1) {
                 board[player.y][player.x] = 2;
@@ -290,4 +369,16 @@ export async function move(dirction) {
             $(findY).find(findX).attr('id', 'player')
         }, speed)
     }
+}
+
+export const boardChecker = function () {
+    for (let i = 0; i < y; i++) {
+        for (let j = 0; j < x; j++) {
+            if (board[i][j] == 1) {
+                return player.won = false;
+
+            }
+        }
+    }
+    return player.won = true;
 }
